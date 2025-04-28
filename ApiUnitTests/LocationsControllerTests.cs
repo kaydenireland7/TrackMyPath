d0e90f775461b6ec9178aed1fbb4a6e2d118a579
Kayden Ireland <kaydenireland@aol.com>
2025-04-28 18:08:23 -0400

    Initial Xunit Setup

---
+++ ApiUnitTests/LocationsControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+
+using PathAPI.Controllers;
+using PathAPI.Models;
+
+namespace ApiUnitTests
+{
+
+    public class LocationsControllerTests
+    {
+
+        private readonly Mock<TrackMyPathContext> _mockContext;
+        private readonly Mock<DbSet<Location>> _mockDbSet;
+
+        public LocationsControllerTests() { 
+            _mockDbSet = new Mock<DbSet<Location>>();
+
+            _mockContext = new Mock<TrackMyPathContext>();
+            _mockContext.Setup(m => m.Locations).Returns(_mockDbSet.Object);
+        }
+    }
+}
